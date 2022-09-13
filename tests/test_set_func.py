7203fe9574c98175fe5f1e27b1ab0ca0c141a356
Ken760 <49761163+Ken760@users.noreply.github.com>
2022-09-13 13:24:34 +0300

    add tests

--- tests/test_set_func.py
+++ tests/test_set_func.py
@@ -3,14 +3,33 @@ import pytest
 
 @pytest.mark.parametrize("set_, expected_result", [
                                                     [type({1, 1, 2, 2, 3, 4, 5, 6}), set],
-                                                    [type({1, 1, 2, 2, 3, 4, 5, 6}), set],
-                                                    [type({1, 1, 2, 2, 3, 4, 5, 6}), set],
+                                                    [type({'apple', 'apple', 'cherry'}), set],
+                                                    [type({1, 6}), set],
                                                     ])
-def test_passing(set_, expected_result):
+def test_for_type(set_, expected_result):
     """Проверка на тип данных"""
 
     assert set_ == expected_result
 
 
+@pytest.mark.parametrize("set_, expected_result",  [
+                                    [{1, 1, 1, 4, 4}, {1, 1, 1, 4}],
+                                    [{'apple', 'apple', 'cherry'}, {'apple', 'cherry'}],
+                                    [{'test', 'test', 1, 1, 1}, {'test', 1}]
+                                    ])
+def test_for_comparison(set_, expected_result):
+    """Проверка на равенсто множеств"""
 
+    assert set_ == expected_result
+
+
+@pytest.mark.parametrize("set_, expected_result",  [
+                                    [{1, 1, 1, 4, 4}, {1, 3, 1, 4}],
+                                    [{'apple', 'apple', 'cherry'}, {'banan', 'cherry'}],
+                                    [{'test', 'test', 1, 1, 1}, {2, 4, 'test', 1}]
+                                    ])
+def test_for_inequality(set_, expected_result):
+    """Проверка на не равенсто множеств"""
+
+    assert set_ != expected_result
 
--- tests/test_tuple_func.py
+++ tests/test_tuple_func.py
@@ -12,3 +12,24 @@ def test_for_type(tuples, expected_result):
     assert tuples == expected_result
 
 
+@pytest.mark.parametrize("tuples, expected_result",  [
+                                    [(1, 2, 3, 4), 1],
+                                    [("apple", "banana", "cherry"), 'apple'],
+                                    [("test", 1, {(1, 1, 1) : 1}, 2, 3), 'test']
+                                    ])
+def test_for_elements(tuples, expected_result):
+    """Проверка нулевого элементов кортежа"""
+
+    assert tuples[0] == expected_result
+
+
+@pytest.mark.parametrize("tuples, expected_result",  [
+                                    [(1, 2, 3, 4), (4, 3, 2, 1)],
+                                    [("apple", "banana", "cherry"), ("chery", "apple", "banana")],
+                                    [("test", 1, {(1, 1, 1) : 1}, 2, 3), (3, 2, {(1, 1, 1): 1}, "test")]
+                                    ])
+def test_for_inequality(tuples, expected_result):
+    """Проверка на не равенсто кортежей"""
+
+    assert tuples != expected_result
+
